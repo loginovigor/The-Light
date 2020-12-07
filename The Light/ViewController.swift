1d27c067603488e8c0f6ceae10df6d2ebbef81dc
Igor Loginov <loguinov.i@gmail.com>
2020-12-07 20:58:54 +0300

    Added toggleTorch

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -6,28 +6,71 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
-    var isLightOn = true
+    var lightColor = 0
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        toggleTorch(on: true)
+        
+        
     }
-
-
+    
+    
     fileprivate func updateUI() {
-      
-    view.backgroundColor = isLightOn ? .white : .black
+        
+        switch lightColor {
+        case 0:
+            view.backgroundColor = .red
+        case 1:
+            view.backgroundColor = .green
+        case 2:
+            view.backgroundColor = .yellow
+            
+        default:
+            print("Error")
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    isLightOn.toggle()
-    updateUI()
+        lightColor = lightColor < 2 ? lightColor + 1 : 0
+        updateUI()
+        toggleTorch(on: true)
+        
     }
-
+    
+    //     вкл фонарика на реальном устройстве
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video)
+        else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+            
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
 }
