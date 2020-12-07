5a5857c05749d6a6dc49f36f1a890196537c3e0e
Igor Loginov <loguinov.i@gmail.com>
2020-12-07 17:51:47 +0300

    Touches began

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -25,10 +25,9 @@ class ViewController: UIViewController {
     view.backgroundColor = isLightOn ? .white : .black
     }
     
-    @IBAction func buttonPressed() {
-        isLightOn.toggle()
-        updateUI()
-
-}
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    isLightOn.toggle()
+    updateUI()
+    }
 
 }
